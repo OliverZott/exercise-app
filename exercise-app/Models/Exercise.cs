3918254ad7d8073bb9856944105e3814b48e07e4
Oliver Zott <zott_oliver@web.de>
2025-02-27 17:31:31 +0100

    #2 Entities - WIP

---
+++ exercise-app/Models/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise_app.Models;
+
+[Table("Exercises")]
+public class Exercise : BaseEntity
+{
+    public DateTime DateTime { get; set; }
+    public int? Diastolic { get; set; }
+    public int? Systolic { get; set; }
+    public int? HeartRate { get; set; }
+    public int? Weight { get; set; }
+    public ExerciseType ExerciseType { get; set; }
+    public string? Notes { get; set; }
+}
---
+++ exercise-app/Models/ExerciseType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise_app.Models;
+
+[Table("ExerciseTypes")]
+public class ExerciseType : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
