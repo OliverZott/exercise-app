3f2fa7f74a4fc2de941181cda6b1e9fa5aefc8a3
Oliver Zott <zott_oliver@web.de>
2025-03-05 11:44:18 +0100

    Enum display converter

---
+++ exercise-app/Utils/ExerciseTypeDisplayConverter.cs
@@ -0,0 +1,42 @@
+﻿using exercise_app.Models;
+using System.Globalization;
+
+namespace exercise_app.Utils;
+
+public class ExerciseTypeDisplayConverter : IValueConverter
+{
+    private static readonly Dictionary<ExerciseType, string> ExerciseTypeDisplayNames = new()
+    {
+        { ExerciseType.Mountainbike, "Mountainbike" },
+        { ExerciseType.Downhill, "Downhill" },
+        { ExerciseType.WeightLifting, "Weight Lifting" },
+        { ExerciseType.TrailRunning, "Trail Running" },
+        { ExerciseType.NordicSkiing, "Nordic Skiing" },
+        { ExerciseType.AlpineSkiing, "Alpine Skiing" },
+        { ExerciseType.SkiTouring, "Ski Touring" },
+        { ExerciseType.Snowboarding, "Snowboarding" },
+        { ExerciseType.Hiking, "Hiking" }
+    };
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IEnumerable<ExerciseType> exerciseTypes)
+        {
+            return exerciseTypes
+                .Select(e => ExerciseTypeDisplayNames.TryGetValue(e, out var displayName) ? displayName : e.ToString())
+                .ToList();
+        }
+
+        if (value is ExerciseType exerciseType && ExerciseTypeDisplayNames.TryGetValue(exerciseType, out var singleDisplayName))
+        {
+            return singleDisplayName;
+        }
+
+        return value?.ToString() ?? string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
